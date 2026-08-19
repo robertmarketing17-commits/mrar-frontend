7c310f77f285b040082f3dad54338cdcacd29ddb
robertmarketing17-commits <robertmarketing17@gmail.com>
2026-08-18 23:19:38 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./main.jsx",
+    "./*/.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
